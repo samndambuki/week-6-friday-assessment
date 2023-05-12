f124f445bbe470bc3270e836541fc11d2f8e204a
samndambuki <sam.ndambuki08@gmail.com>
2023-05-12 10:55:10 +0300

    finished encryption

--- src/controllers/usersContoller.ts
+++ src/controllers/usersContoller.ts
@@ -2,19 +2,15 @@ import  { Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import mssql from 'mssql';
 import {sqlConfig} from "../config";
+import Joi from 'joi';
+import bcrypt from 'bcrypt';
 
-
-// const app = express();
-// app.use(express.json());
-
-// Endpoint for registering a new user
-// app.post('/register', async (req: Request, res: Response)
 
 interface ExtendedRequest extends Request{
     body:{
-        name:String
-        email:String
-        password:String
+        name:string
+        email:string
+        password:string
     }
 }
 
@@ -22,19 +18,31 @@ export const registerUser = async (req:ExtendedRequest,res:Response) => {
   try {
     const { name, email, password } = req.body;
 
-    // Generate a unique ID for the user
+    // Validate request body using Joi
+    const schema = Joi.object({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(8).required(),
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
     const id = uuidv4();
 
-    // Connect to the database
+    // Encrypt the password using bcrypt
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const pool = await mssql.connect(sqlConfig);
 
-    // Execute the database query to insert the user
     await pool
       .request()
       .input('id', mssql.VarChar, id)
       .input('name', mssql.VarChar, name)
       .input('email', mssql.VarChar, email)
-      .input('password', mssql.VarChar, password)
+      .input('password', mssql.VarChar, hashedPassword)
       .execute('RegisterUser');
 
     return res.status(201).json({ message: 'User registered successfully' });
